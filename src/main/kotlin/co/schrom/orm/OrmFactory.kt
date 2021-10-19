634250f130528fb6a8bf9b7c1cb26d7febbe9ced
Moritz Schrom <moritz.schrom@schrom.co>
2021-10-19 09:58:18 +0200

    Added initialization process for the ORM

--- src/main/kotlin/co/schrom/orm/OrmFactory.kt
+++ src/main/kotlin/co/schrom/orm/OrmFactory.kt
@@ -1,7 +1,15 @@
 package co.schrom.orm
 
+import java.sql.Connection
+import java.sql.DriverManager
+
 abstract class OrmFactory {
 
-    abstract fun createOrm(): Orm
+    abstract fun createOrm(connection: Connection): Orm
+
+    fun createOrm(url: String, user: String, password: String): Orm {
+        val conn = DriverManager.getConnection(url, user, password)
+        return this.createOrm(conn)
+    }
 
 }
